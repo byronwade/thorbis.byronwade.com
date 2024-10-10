5e07a8067a289b7a2253f839f4247ab9be238b87
Byron Wade <bcw1995@gmail.com>
2024-10-09 21:10:16 -0400

    Here is a concise commit message for the given git diff:
    **Fixed default data structure in Example Blueprint**
    
    Modified `blueprint-config.js` to remove unnecessary `data` property and update `posts` array with a single item.

--- blueprints/example-blueprint copy/blueprint-config.js
+++ blueprints/example-blueprint copy/blueprint-config.js
@@ -2,13 +2,4 @@ module.exports = {
     name: "Example Blueprint",
     layouts: ["default", "blog"],
     components: ["header", "footer", "blogPost"],
-    data: {
-        posts: [
-            {
-                title: "My First Post",
-                slug: "my-first-post",
-                content: "This is the body of my first post."
-            }
-        ]
-    }
 };
