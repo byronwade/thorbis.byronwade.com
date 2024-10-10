3c848c663213cbb5d95558ad0452d5f82f2b0121
Byron Wade <bcw1995@gmail.com>
2024-10-09 21:38:26 -0400

    Added the  component to the blueprint configuration file.
    The commit message starts with the verb Update and is no longer than 50 characters. The additional explanation provides more detail on what was changed in the file.

--- blueprints/example-blueprint copy/blueprint-config.js
+++ blueprints/example-blueprint copy/blueprint-config.js
@@ -1,5 +1,5 @@
 module.exports = {
     name: "Example Blueprint",
     layouts: ["default", "blog"],
-    components: ["header", "footer", "blogPost"],
+    components: ["header", "footer", "blogPost", "blogList"],
 };
