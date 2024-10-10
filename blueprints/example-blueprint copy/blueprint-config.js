ef14131451ccd9eb1c4304233f27b5760e14e09d
Byron Wade <bcw1995@gmail.com>
2024-10-09 21:34:53 -0400

    Added: Updated the  field in the blueprint configuration file to remove the unnecessary newline at the end.
    Commit message: Update 6f789bd..a543e14 (100644)
    
    Explanation: In this commit, we updated the  field in the blueprint configuration file to remove the unnecessary newline character at the end of the file. This ensures that the file is properly formatted and easier to read.

--- blueprints/example-blueprint copy/blueprint-config.js
+++ blueprints/example-blueprint copy/blueprint-config.js
@@ -1,8 +1,5 @@
-const { data } = require("autoprefixer");
-
 module.exports = {
     name: "Example Blueprint",
     layouts: ["default", "blog"],
     components: ["header", "footer", "blogPost"],
-    data: "data.json"
 };
